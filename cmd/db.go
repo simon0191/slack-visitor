41b53d98b9f6a9182c1254fd49cc01b163f0b3f2
Simon Soriano <simon0191@gmail.com>
2017-04-17 00:10:32 -0500

    Add Rollback command

--- cmd/db.go
+++ cmd/db.go
@@ -8,11 +8,13 @@ import (
 	"github.com/simon0191/slack-visitor/utils"
 	"github.com/spf13/cobra"
 	"gopkg.in/gormigrate.v1"
+	"time"
 )
 
 func init() {
 	dbCmd.AddCommand(
 		dbMigrateCmd,
+		dbRollbackCmd,
 	)
 }
 
@@ -25,10 +27,92 @@ var dbMigrateCmd = &cobra.Command{
 	Run: dbMigrateCmdFunc,
 }
 
+var dbRollbackCmd = &cobra.Command{
+	Use: "rollback",
+	Run: dbRollbackCmdFunc,
+}
+
+//TODO: place each migration in a different file
+var migrations = []*gormigrate.Migration{
+	// load pgcrypto extension
+	{
+		ID: "201704160930",
+		Migrate: func(tx *gorm.DB) error {
+			return tx.Exec("CREATE EXTENSION pgcrypto;").Error
+		},
+		Rollback: func(tx *gorm.DB) error {
+			return tx.Exec("DROP EXTENSION IF EXISTS pgcrypto;").Error
+		},
+	},
+	// create chat request states table
+	{
+		ID: "201704161038",
+		Migrate: func(tx *gorm.DB) error {
+			type ChatRequestState struct {
+				ID string `gorm:"primary_key;type:varchar(100)"`
+			}
+			if err := tx.AutoMigrate(&ChatRequestState{}).Error; err != nil {
+				return err
+			}
+			for _, state := range []string{"pending", "accepted", "declined"} {
+				if err := tx.Create(&ChatRequestState{ID: state}).Error; err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+		Rollback: func(tx *gorm.DB) error {
+			return tx.DropTable("chat_request_states").Error
+		},
+	},
+	// create chat requests table
+	{
+		ID: "201704161040",
+		Migrate: func(tx *gorm.DB) error {
+
+			type ChatRequest struct {
+				ID        string `gorm:"primary_key;type:uuid;default:gen_random_uuid()"`
+				State     string `gorm:"primary_key;type:varchar(100)"`
+				CreatedAt time.Time
+				UpdatedAt time.Time
+				DeletedAt *time.Time
+			}
+
+			if err := tx.AutoMigrate(&ChatRequest{}).Error; err != nil {
+				return err
+			}
+
+			return tx.Model(ChatRequest{}).AddForeignKey("state", "chat_request_states (id)", "RESTRICT", "RESTRICT").Error
+		},
+		Rollback: func(tx *gorm.DB) error {
+			return tx.DropTable("chat_requests").Error
+		},
+	},
+}
+
 func dbMigrateCmdFunc(cmd *cobra.Command, args []string) {
+	m := initMigrations(cmd)
+	if err := m.Migrate(); err != nil {
+		log.Fatalf("Could not migrate: %v", err)
+	}
+
+	log.Printf("Migration did run successfully")
+}
+
+func dbRollbackCmdFunc(cmd *cobra.Command, args []string) {
+	//TODO: implement Rollback specifying migration with m.RollbackMigration(migration)
+	m := initMigrations(cmd)
+	if err := m.RollbackLast(); err != nil {
+		log.Fatalf("Could not rollback: %v", err)
+	}
+
+	log.Printf("Rollback did run successfully")
+}
+
+func initMigrations(cmd *cobra.Command) *gormigrate.Gormigrate {
 	configPath, err := cmd.Flags().GetString("config")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	config, err := utils.LoadConfig(configPath)
@@ -46,50 +130,5 @@ func dbMigrateCmdFunc(cmd *cobra.Command, args []string) {
 
 	db.LogMode(true)
 
-	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
-		// create chats table
-		{
-			ID: "201704091117",
-			Migrate: func(tx *gorm.DB) error {
-				type Chat struct {
-					gorm.Model
-					UID       string `sql:"index"`
-					channel   string
-					VisitorID uint `sql:"index"`
-				}
-				return tx.AutoMigrate(&Chat{}).Error
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.DropTable("chats").Error
-			},
-		},
-		// create messages table
-		{
-			ID: "201704091119",
-			Migrate: func(tx *gorm.DB) error {
-				type Message struct {
-					gorm.Model
-					Text     string
-					ChatID   uint
-					UserType uint
-					UserID   string
-				}
-
-				if err := tx.AutoMigrate(&Message{}).Error; err != nil {
-					return err
-				}
-
-				return tx.Model(Message{}).AddForeignKey("chat_id", "chats (id)", "RESTRICT", "RESTRICT").Error
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.DropTable("messages").Error
-			},
-		},
-	})
-
-	if err = m.Migrate(); err != nil {
-		log.Fatalf("Could not migrate: %v", err)
-	}
-
-	log.Printf("Migration did run successfully")
+	return gormigrate.New(db, gormigrate.DefaultOptions, migrations)
 }
