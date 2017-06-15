355038536aeb15c48f36c6799d9136ac10ece36f
Simon Soriano <simon0191@gmail.com>
2017-06-14 23:44:40 -0500

    Fix client deployment

--- client/environment/prod.env.js
+++ client/environment/prod.env.js
@@ -2,7 +2,7 @@ module.exports = {
   ENV: '"production"',
   NODE_ENV: '"production"',
   DEBUG_MODE: true,
-  API_URL: '"https://slackvisitor.herokuapp.com"',
-  API_HOST_NAME: '"https://slackvisitor.herokuapp.com"',
-  API_PORT: '80',
+  API_URL: `"${process.env.API_URL || 'http://localhost:8000'}"`,
+  API_HOST_NAME: `"${process.env.API_HOST_NAME || 'http://localhost'}"`,
+  API_PORT: `${process.env.API_PORT || 8000}`,
 }
